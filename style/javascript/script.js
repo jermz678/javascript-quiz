1288d9e9fe93b6c28f79e2407d6a9bb46aa7a497
jermz678 <jermz678@live.com>
2021-03-28 15:12:44 -0500

    reworking time so it would end quiz properly

--- style/javascript/script.js
+++ style/javascript/script.js
@@ -5,6 +5,7 @@ var btn1 = document.getElementById("a");
 var btn2 = document.getElementById("b");
 var btn3 = document.getElementById("c");
 var btn4 = document.getElementById("d");
+var questionDiv = document.getElementById("question-div");
 var startQuiz = document.getElementById("quiz-starter");
 var proposedQuestion = document.getElementById("question-holder");
 var userScore = document.getElementById("userscore");
@@ -69,68 +70,76 @@ var questions = [
 var answers = [ "d", "a", "c", "c", "b"];
  
 //variable to hold score
-updatedScore = 0;
+var highScore = 0
+var updatedScore = 0;
 userScore.innerText = "Score " + updatedScore;
 
 //variable to hold time left on clock
-var timeleft
+var timeLeft = 0;
 
-//function to end quiz
-function endQuiz() {
-    
-    
-        proposedQuestion.innerText = "Great Job! Save your score and see if you are in the top 3!!"
-        btn1.remove();
-        btn2.remove();
-        btn3.remove();
-        btn4.remove();
-        feedback.innerHTML = "";
-    
-}
 //create timer function
 function countDown(){
-    timeLeft = 5;
+    timeLeft = 50;
     var interval = setInterval(function(){
     timeLeft--;
     timerDisplay.innerHTML = "Time Left " + timeLeft; 
-            if ( timeLeft === 0){
-                endQuiz();
+            if ( timeLeft <= 0){
                 clearInterval(interval);
+                endQuiz();
             }
     }, 1000);
 }
 
+//function to end quiz
+function endQuiz() {
+    proposedQuestion.innerText = "Great Job! Save your score and see if you are in the top 3!!"
+    btn1.remove();
+    btn2.remove();
+    btn3.remove();
+    btn4.remove();
+    feedback.innerHTML = "";
+    console.log(highScore);
+    localStorage.setItem(highScore, updatedScore);
+   // var saveHighScore = document.createElement("FORM");
+    //saveHighScore.innertext = "whatsups dfapsoasj";
+    //questionDiv.appendChild(saveHighScore);
+
+}
+
 //checking if answer is correct and going to next question
 function checkAnswer(event){
-    
     if(event.target.id === answers[answerIndex]){
         updatedScore += 10;
         userScore.innerText = "Score " + updatedScore;
         feedback.innerHTML = "Correct! 👍";
-    
+        console.log(timeLeft)
     }
     else{
         timeLeft -= 10;
         feedback.innerHTML = "Wrong 👎";
-        
-    }
+    }  
     questionIndex++;
     answerIndex++;
-    
+    if( questionIndex === 5){
+        endQuiz();
+    }
     proposedQuestion.innerText = questions[questionIndex].q;
     btn1.innerText = questions[questionIndex].a[0].text;
     btn2.innerText = questions[questionIndex].a[1].text;
     btn3.innerText = questions[questionIndex].a[2].text;
     btn4.innerText = questions[questionIndex].a[3].text;  
+
     
-    if ( questionIndex === 4 || timeLeft >= 0){endQuiz();
-};
+    
 }
+
+
 
  //beginning first question
 function timeForQuestions(){
     startQuiz.remove();
-    countDown(); 
+    countDown();
+    
     for(var i = 0; i < questions.length; i++){
     proposedQuestion.innerText = questions[questionIndex].q;
     btn1.innerText = questions[questionIndex].a[0].text;
@@ -138,6 +147,7 @@ function timeForQuestions(){
     btn3.innerText = questions[questionIndex].a[2].text;
     btn4.innerText = questions[questionIndex].a[3].text;
     }
+    
 }    
     
     
