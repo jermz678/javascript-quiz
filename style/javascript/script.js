2df531f07c6aa03c10c9a14a996b335db5065f08
jermz678 <jermz678@live.com>
2021-03-28 10:07:37 -0500

    made a seperate element to hold the question vs start button quiz, made start button disapear when quiz starts

--- style/javascript/script.js
+++ style/javascript/script.js
@@ -5,12 +5,13 @@ var btn1 = document.getElementById("a");
 var btn2 = document.getElementById("b");
 var btn3 = document.getElementById("c");
 var btn4 = document.getElementById("d");
+var startQuiz = document.getElementById("quiz-starter");
 var proposedQuestion = document.getElementById("question-holder");
 var userScore = document.getElementById("userscore");
 var timerDisplay = document.getElementById("timer");
 
 //array to hold the questions as objects
-var question1 = [
+var questions = [
     {
         q: "What is a string",
         a: [
@@ -19,9 +20,7 @@ var question1 = [
             { text: "numbers" },
             { text: "is 0 or more characters within quotes"}
         ]
-    }
-]
-var question2 = [
+    },
     {
         q: "!!What is a string",
         a: [
@@ -30,9 +29,7 @@ var question2 = [
             { text: "numbers"},
             { text: "is 0 or more characters within quotes"}
         ]
-    }
-]
-var question3 = [
+    },
     {
         q: "!!!What is a string",
         a: [
@@ -43,7 +40,7 @@ var question3 = [
         ]
     }
 ];
-console.log(question3)
+
 // array to hold answers
 
  var answers = [ "a", "b", "c", "d"];
@@ -54,15 +51,7 @@ console.log(question3)
 updatedScore = 0;
 userScore.innerText = "Score " + updatedScore;
 
-//variable to hold correct answer
-//var correctAnswer = question1[questionIndex].a[3].text;
-//console.log(correctAnswer)
 
-//beginning of quiz
-function startQuiz (){
-    proposedQuestion.innerText = "Start the Quiz"
-}
-startQuiz();
 
 //create timer function
 function countDown(){
@@ -80,22 +69,18 @@ function checkAnswer(event){
     
    if(event.target.id === answers[3]){
        alert("yesh");
-        updatedScore + 10;
+        
    }
-
-    console.log(event.target.id)
-    console.log(answers[3])
-
-
-
 }
 
 function timeForQuestions(){
-    proposedQuestion.innerText = question1[questionIndex].q;
-    btn1.innerText = question1[questionIndex].a[0].text;
-    btn2.innerText = question1[questionIndex].a[1].text;
-    btn3.innerText = question1[questionIndex].a[2].text;
-    btn4.innerText = question1[questionIndex].a[3].text;
+    startQuiz.remove();
+    for(var i = 0; i < questions.length; i++){
+    proposedQuestion.innerText = questions[questionIndex].q;
+    btn1.innerText = questions[questionIndex].a[0].text;
+    btn2.innerText = questions[questionIndex].a[1].text;
+    btn3.innerText = questions[questionIndex].a[2].text;
+    btn4.innerText = questions[questionIndex].a[3].text;
     countDown();  
     btn1.addEventListener("click", function(event){
         checkAnswer(event);
@@ -103,16 +88,23 @@ function timeForQuestions(){
     btn2.addEventListener("click", function(event){
         checkAnswer(event);
     });
-    btn3.addEventListener("click", function(event){
-        checkAnswer(event);
+   btn3.addEventListener("click", function(event){
+       checkAnswer(event);
     });
     btn4.addEventListener("click", function(event){
         checkAnswer(event);
     });
-}
+    
+    
+}}
+
+
+
+
+
 
 //button event listener
-proposedQuestion.addEventListener("click", function(){
+startQuiz.addEventListener("click", function(){
     timeForQuestions();
 }
 );
